31a77cb18969a2d800f670cfb5631db691c63163
Doug DeJong <dougdejong@gmail.com>
2018-11-10 14:58:44 -0500

    added temporary landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import myface from './myface.jpg'
 import './App.css';
 
 class App extends Component {
@@ -7,17 +8,19 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={myface} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+            Hi! I'm Douglas DeJong.          </p>
+            <p>I'm a full-stack Web Developer specializing in the MERN Stack</p>
+            <p>I'm also a...</p>
+            <p>Type 1 Diabetic | Music Producer | EA Gamechanger | Hockey Lover</p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://github.com/DougDeJong"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            My Github
           </a>
         </header>
       </div>
