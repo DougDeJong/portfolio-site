f156992e6365858ebb4f831a1693401a184ad2d0
Doug DeJong <dougdejong@gmail.com>
2018-11-27 01:08:26 -0500

    added some more html

--- src/components/Hockey.js
+++ src/components/Hockey.js
@@ -7,6 +7,10 @@ const Hockey = () => {
 
     return(
       <div>
+        <p>In my spare time, I play a lot of ice hockey.  Both in the real world and in the digital realm...</p>
+        <p>Maybe a little bit more in the digital realm.  I'm part of the exclusive EA Gamechanger program in which EA provides me with games and content in order to keep producing my
+          own content on twitch!!! 
+        </p>
       <TwitterTimelineEmbed
       sourceType="profile"
       screenName="TheDeJtv"
